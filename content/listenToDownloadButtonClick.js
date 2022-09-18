af80a27cc049357b01cce1d906a869062efc8759
Bill Enriquez <raregem07@gmail.com>
2022-09-18 20:59:14 +0800

    updated the download logic

--- content/listenToDownloadButtonClick.js
+++ content/listenToDownloadButtonClick.js
@@ -1,9 +1,9 @@
 downloadButton = document.getElementById("downloadButton");
 
-downloadButton.addEventListener("click", async () => {
+downloadButton.addEventListener("click", () => {
     
     chrome.storage.local.get("total_rows_processed", ({ total_rows_processed }) => {
-        csvContent = "data:text/csv;charset=utf-8;";
+        csvContent = "data:text/csv;charset=utf-8;,";
         /* + total_rows_processed.map(row => {
             return (row.title!=="undefined"?row.title:" " + "," + row.email!=="undefined"?row.email:" " + "," + row.profile_url!=="undefined"?row.profile_url:" " + "\r\n");  
         }); */
@@ -17,17 +17,40 @@ downloadButton.addEventListener("click", async () => {
             csvContent += row.profile_url !== "undefined" ? "\"" + row.profile_url + "\"" : "\"\"";
             csvContent += "\r\n";
         });
-        var encodedUri = encodeURI(csvContent);
-        var link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "sm_extracted_data.csv");
-        document.body.appendChild(link); // Required for FF
-    
-        link.click(); // This will download the data file named "sm_extracted_data.csv".
-        chrome.storage.local.set({total_rows_processed: []});
-        processed_rows = 0;
-        chrome.storage.local.set({ processed_rows });
-        document.getElementById("scraped_total_count").innerText = "0";
+
+        ///////// PROCEDURE 1 /////////////
+        let encodedUri = encodeURI(csvContent);
+        //let link = document.createElement("a");  // let's just refer to existing element
+        let link = document.getElementById("forCsvFileDownload");
+        if (link.tagName === "A" && chrome.runtime && !!chrome.runtime.getManifest() /* isValidChromeRuntime() */ ) {
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "sm_extracted_data.csv");
+            console.log("about to download the csv file");
+            //document.body.appendChild(link); // Required for FF (but it already exists)
+            link.click(); // This will download the data file named "sm_extracted_data.csv".
+            //=====
+
+            ////////// PROCEDURE 2 ///////////////
+            // let blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+            // let url = URL.createObjectURL(blob);
+            // chrome.downloads.download({
+            //     url: url,
+            //     filename: "sm_extracted_data.csv"
+            // });
+            //=====
+
+            chrome.storage.local.set({total_rows_processed: []});
+            processed_rows = 0;
+            chrome.storage.local.set({ processed_rows });
+            document.getElementById("scraped_total_count").innerText = "0";
+        } else {
+            alert("Something went wrong. No file was downloaded.");
+        }
     });
 
 });
+
+
+/* const isValidChromeRuntime = () => {
+    return chrome.runtime && !!chrome.runtime.getManifest();
+} */
